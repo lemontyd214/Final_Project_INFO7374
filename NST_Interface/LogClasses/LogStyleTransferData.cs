4d8c763aa3c4cf0304e9322798b4c1543f299988
lemontyd214 <teng.y@husky.neu.edu>
2018-04-26 23:50:13 -0400

    Added Interface

---
+++ NST_Interface/LogClasses/LogStyleTransferData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neural_Dream
+{
+    class LogStyleTransferData
+    {
+        public string Time { get; set; }
+        public string ScriptType { get; set; }
+        public string ContentFilePath { get; set; }
+        public string StyleFilePath { get; set; }
+        public string OutputFilePrefix { get; set; }
+        public string ParameterList { get; set; }
+    }
+}
